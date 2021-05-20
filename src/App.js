26b5c595f9fcc695c69700369bbb67877d580a71
Luiggy Macias <ferrinluiggy@gmail.com>
2021-05-20 11:53:18 -0500

    usando sass con react :)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 //import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
+//components
 
-function App() {
-  return (
-    
-  );
+import Header from './components/Header';
+
+class App extends Component {
+
+	render() {
+		return <div>
+    	<Header />
+    	</div>
+	}
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import './css/header.css'
+
+class Header extends Component {
+	
+
+	render() {
+		return <header>
+		</header>
+	}
+}
+
+export default Header;
