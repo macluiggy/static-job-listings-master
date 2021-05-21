01cd2a17c523c1988f7c21a570338633f45141ea
Luiggy Macias <ferrinluiggy@gmail.com>
2021-05-21 09:50:57 -0500

    intentendo filtrar contenido

--- src/components/main.js
+++ src/components/main.js
@@ -5,20 +5,31 @@ import photosnap from './images/photosnap.svg';
 import manage from './images/manage.svg';
 
 export default class Main extends Component {
-	constructor(props) {
-		super(props)
+	
+	filterSkill = (e) => {
+		//console.log(e.currentTarget)
+		let skill = e.target.innerText;
+		//console.log(skill);
+		let filteredSkill = this.props.data.find(person => {
+			return skill === person.role || skill === person.level || skill === person.languages.find(language => language == skill)
+		})
+		console.log(filteredSkill)
+		this.appendFilter(filteredSkill)
+	}
+
+	appendFilter = (filteredSkill) => {
+		return <li>{filteredSkill}</li>
 	}
 
 	render() {
-		const {data} = this.props.data;
+		const data = this.props.data;
+		console.log(data)
 		return (<main>
 			<div className="filter_container">
 				<ul className="skills">
-                	<li className="role">Frontend <span className="X">X</span></li>
+                	<li className="role" >Frontend<span className="X">X</span></li>
                 	<li className="level">Senior</li>
-                	<li className="languages">HTML</li>
-                	<li className="languages">CSS</li>
-                	<li className="languages">JavaScript</li>
+                	{this.appendFilter}
             	</ul>
             	<span className="clear">Clear</span>
 			</div>
@@ -27,8 +38,8 @@ export default class Main extends Component {
                 <figure className="logo">
                     <img src={photosnap} alt="photosnap logo" />
                 </figure>
-                <article class="description">
-                    <div class="logo_name">
+                <article className="description">
+                    <div className="logo_name">
                         <span className="logo_name_span">Photosnap</span>
                         <span className="new">New!</span>
                         <span className="featured">Featured</span>
@@ -42,20 +53,20 @@ export default class Main extends Component {
                 </article>
             </div>
             <ul className="skills">
-                <li className="role">Frontend</li>
-                <li className="level">Senior</li>
+                <li className="role" onClick={this.filterSkill}>Frontend</li>
+                <li className="level" onClick={this.filterSkill}>Senior</li>
                 <li className="languages">HTML</li>
                 <li className="languages">CSS</li>
                 <li className="languages">JavaScript</li>
             </ul>
         </section>
-        <section className="items item1">
+        <section className="items item2">
             <div className="logo_description">
                 <figure className="logo">
-                    <img src={manage} />
+                    <img src={manage} alt="manage logo"/>
                 </figure>
-                <article class="description">
-                    <div class="logo_name">
+                <article className="description">
+                    <div className="logo_name">
                         <span className="logo_name_span">Manage</span>
                         <span className="new">New!</span>
                         <span className="featured">Featured</span>
