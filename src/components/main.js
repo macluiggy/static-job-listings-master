a8bb979fac71ee8a85f98b7b42967a8461e30c2e
Luiggy Macias <ferrinluiggy@gmail.com>
2021-05-21 15:04:23 -0500

    intentando mostrar u ocultar el contenido

--- src/components/main.js
+++ src/components/main.js
@@ -1,52 +1,65 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
+import { $$, $, p } from './functions.js'
 //images
 import photosnap from './images/photosnap.svg';
 import manage from './images/manage.svg';
 
 import Filter from './Filter';
 
+let skills = $$('.skills').childNodes;
+
 export default class Main extends Component {
 
-	state = {
-		skills: []
-	}
+    state = {
+        skills: [],
+        cardContainer: "items item1",
+        none: "none"
+    }
 
-	filterSkill = (e) => {
-		//console.log(e.currentTarget)
-		let skill = e.target.innerText;
-		console.log(this.props.data)
-		//console.log(skill);
-		for(let i = 0, length1 = this.props.data.length; i < length1; i++){
-			let person = this.props.data[i]
-			if (person.role === skill || person.level === skill) {
-				// statement
-				this.setState({
-					skills: [...this.state.skills, skill]
-				})
-			}
-		}
-		//this.appendFilter(filteredSkill)
-	}
+    filterSkill = (e) => {
+        //console.log(e.currentTarget)
+        let skill = e.target.innerText;
+        console.log(this.props.data)
+        //console.log(skill);
+        for (let i = 0, length1 = this.props.data.length; i < length1; i++) {
+            let person = this.props.data[i]
+            if (person.role === skill || person.level === skill) {
+                // statement
+                this.setState({
+                    skills: [...this.state.skills, skill]
+                })
+            }
+            for (let i = 0, length1 = person.languages.length; i < length1; i++) {
+                let language = person.languages[i]
+                if (language === skill) {
+                    // statement
+                    this.setState({
+                        skills: [...this.state.skills, skill]
+                    })
+                }
+            }
+        }
+    }
 
-	
 
-	render() {
-		const data = [...new Set(this.state.skills)]
-		console.log(data)
-		//console.log(data)
-		return (<main>
-			<div className="filter_container">
+
+    render() {
+        const skills = [...new Set(this.state.skills)]
+        console.log(skills)
+        //console.log(data)
+        return (<main>
+			<div className='filter_container'>
 				<ul className="skills">
                 	<li className="role" >Frontend<span className="X">X</span></li>
                 	<li className="level">Senior</li>
-                	{data.map(skill => {
-                		return <li>{skill}</li>
+                	{skills.map(skill => {
+                		return <li key={skill.id}>{skill}</li>
                 	})}
             	</ul>
             	<span className="clear">Clear</span>
 			</div>
-			<section className="items item1">
+			<section className={this.state.cardContainer}>
             <div className="logo_description">
                 <figure className="logo">
                     <img src={photosnap} alt="photosnap logo" />
@@ -68,12 +81,12 @@ export default class Main extends Component {
             <ul className="skills">
                 <li className="role" onClick={this.filterSkill}>Frontend</li>
                 <li className="level" onClick={this.filterSkill}>Senior</li>
-                <li className="languages">HTML</li>
-                <li className="languages">CSS</li>
-                <li className="languages">JavaScript</li>
+                <li className="languages" onClick={this.filterSkill}>HTML</li>
+                <li className="languages" onClick={this.filterSkill}>CSS</li>
+                <li className="languages" onClick={this.filterSkill}>JavaScript</li>
             </ul>
         </section>
-        <section className="items item2">
+        <section className={this.state.cardContainer}>
             <div className="logo_description">
                 <figure className="logo">
                     <img src={manage} alt="manage logo"/>
@@ -100,5 +113,5 @@ export default class Main extends Component {
             </ul>
         </section>
 		</main>)
-	}
+    }
 }
